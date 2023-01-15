52a3311741bb8796cec5f13ca157edd36bf7de30
HouserL <91702660+HouserL@users.noreply.github.com>
2023-01-14 18:48:47 -0600

    Removed Old Files and Folders from Repository.
    Removed old files and paths no longer needed in repository.
    Fixed Name on Folder for CustomRandomizer not to includ spaces.

--- Custom Randomizer/Models/LoadOutModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CustomRandomizer.Models;
-public class LoadOutModel
-{
-    public string Name { get; set; } = string.Empty;
-    public List<string> Tables { get; set; } = new();
-}
--- Custom Randomizer/Models/TableItemModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CustomRandomizer.Models;
-public class TableItemModel
-{
-    public int Weight { get; set; }
-    public string Value { get; set; } = string.Empty;
-    public List<string> TableNames { get; set; } = new();
-    public bool IsTable { get; set; } = false;
-}
